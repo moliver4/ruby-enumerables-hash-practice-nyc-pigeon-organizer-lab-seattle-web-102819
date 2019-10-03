5a54bc02ce67b08188f9bb7641d35cc9f8498b1a
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 23:11:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.map{} do |trait, attributes|
+  data.map do |trait, attributes|
     attributes.each do |details, names|
       names.each do |name|
         pigeon_list[name] ||= {}
